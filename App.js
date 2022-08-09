3fabb3890b42a1c8b13063423d5c59a8e001c620
Xavier <kaisayshi12@gmail.com>
2022-08-08 20:03:53 -0700

    starts basic mapping to see if user has notes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import React, { useEffect } from'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import PopOut from './components/PopOut';
@@ -10,14 +11,43 @@ const Stack = createStackNavigator();
 
 
 export default function App() {
-  return (
-    
-    <NavigationContainer  >
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name='PopOut' component={PopOut} /> 
-        <Stack.Screen name='Note' component={NoteScreen}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  const [notes, setNotes] = React.useState([])
+
+  useEffect(() =>{
+    notesList();
+  })
+  const notesList = async () => {
+    //grab notes from async storage
+    const result = await AsyncStorage.getItem('notes');
+  
+    //the empty array thing is confusing so, if result==null, it is empty array (the React.useState was being weird)
+    //otherwise show all notes
+    result ? setNotes(JSON.parse(result)) : setNotes([]);
+  
+  }
+  if (notes.length == 0) {
+    //null show notes 
+    return (
+      <NavigationContainer  >
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name='PopOut' component={PopOut} /> 
+          <Stack.Screen name='Note' component={NoteScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+  else{
+    // not null, show notes
+    return (
+      <NavigationContainer  >
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name='PopOut' component={PopOut} /> 
+          <Stack.Screen name='Note' component={NoteScreen}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+  
 }
 
